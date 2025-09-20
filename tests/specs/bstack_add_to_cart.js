94de4f783dcc888d89eac6f08a478d3f55fac0e3
MwangiN <mwangitm1996@gmail.com>
2025-09-20 13:05:56 +0300

    added login

---
+++ tests/specs/bstack_add_to_cart.js
@@ -0,0 +1,28 @@
+const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../../pages/login-page');
+const { users } = require('../../data/auth/users');
+
+test('login and validate vendors heading and products found', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(users.demoUser.username, users.demoUser.password);
+
+  // Validation steps
+  const vendorsHeading = await page.getByRole('heading', { name: 'Vendors:' });
+  await expect(vendorsHeading).toBeVisible();
+  await vendorsHeading.click();
+
+  const productsFoundText = await page.getByText('Product(s) found.');
+  await expect(productsFoundText).toBeVisible();
+  await productsFoundText.click();
+});
+
+test('Add products to cart', async () => {
+  // Add products to cart
+  await page.locator('[id="\\31 "]').getByText('Add to cart').click();
+  await page.locator('[id="\\32 "]').getByText('Add to cart').click();
+
+  // Validate cart count
+  const cartCount = await page.locator('span').filter({ hasText: '2' }).nth(2);
+  await expect(cartCount).toBeVisible();
+});
