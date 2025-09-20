6b3867c9a85af52d68dad6d378a25510e7e27558
MwangiN <mwangitm1996@gmail.com>
2025-09-20 13:29:00 +0300

    homepage

---
+++ pages/home-page.js
@@ -0,0 +1,52 @@
+// pages/login-page.js
+
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.signInLink = page.getByRole('link', { name: 'Sign In' });
+    this.usernameDropdown = page.locator('div').filter({ hasText: /^Select Username$/ }).nth(2);
+    this.passwordDropdown = page.locator('div').filter({ hasText: /^Select Password$/ }).nth(2);
+    this.loginButton = page.getByRole('button', { name: 'Log In' });
+    this.logoutLink = page.getByRole('link', { name: 'Logout' });
+    this.firstProduct = page.locator('.shelf-item__buy-btn').nth(0);
+    this.secondProduct = page.locator('.shelf-item__buy-btn').nth(1);
+  }
+
+  async goto() {
+    await this.page.goto('https://testathon.live/');
+  }
+
+  async openLoginForm() {
+    await this.signInLink.click();
+  }
+
+  async selectUsername(username) {
+    await this.usernameDropdown.click();
+    await this.page.getByText(username, { exact: true }).click();
+  }
+
+  async selectPassword(password) {
+    await this.passwordDropdown.click();
+    await this.page.getByText(password, { exact: true }).click();
+  }
+
+  async login(username, password) {
+    await this.openLoginForm();
+    await this.selectUsername(username);
+    await this.selectPassword(password);
+    await this.loginButton.click();
+  }
+
+  async logout() {
+    await this.logoutLink.click();
+  }
+
+    async addFirstProductToCart() {
+    await this.firstProduct.click();
+    }
+    async addSecondProductToCart() {
+    await this.secondProduct.click();
+    }
+}
+
+module.exports = { HomePage };
